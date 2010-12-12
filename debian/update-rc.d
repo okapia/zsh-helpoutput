ad085f5fddd2b11b7db794bd38fe03d20fec9708
Oliver Kiddle <opk@zsh.org>
2010-12-12 21:42:00 +0100

    update-rc.d 2.86.ds1-61

---
+++ debian/update-rc.d
@@ -0,0 +1,5 @@
+usage: update-rc.d [-n] [-f] <basename> remove
+       update-rc.d [-n] <basename> defaults [NN | SS KK]
+       update-rc.d [-n] <basename> start|stop NN runlvl [runlvl] [...] .
+		-n: not really
+		-f: force
