a162e36cd892035d4f4efb4efd1e7fe62cfb6305
Oliver Kiddle <opk@zsh.org>
2012-01-20 23:52:37 +0100

    update-rc.d 2.88dsf-22.1

--- debian/update-rc.d
+++ debian/update-rc.d
@@ -1,5 +1,10 @@
+update-rc.d: error: not enough arguments
 usage: update-rc.d [-n] [-f] <basename> remove
        update-rc.d [-n] <basename> defaults [NN | SS KK]
        update-rc.d [-n] <basename> start|stop NN runlvl [runlvl] [...] .
+       update-rc.d [-n] <basename> disable|enable [S|2|3|4|5]
 		-n: not really
 		-f: force
+
+The disable|enable API is not stable and might change in the future.
+update-rc.d: using dependency based boot sequencing
